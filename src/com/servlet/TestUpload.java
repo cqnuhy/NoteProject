a779d5d71e3d824608da091915da81c24caa8ea6
cqnuhy <cqnuhy@qq.com>
2016-04-13 17:45:31 +0800

    更新文件上传说明

--- src/com/servlet/TestUpload.java
+++ src/com/servlet/TestUpload.java
@@ -17,20 +17,21 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  * 
- * @author Administrator
- * 文件上传
- * 具体步骤：
- * 1）获得磁盘文件条目工厂 DiskFileItemFactory 要导包
- * 2） 利用 request 获取 真实路径 ，供临时文件存储，和 最终文件存储 ，这两个存储位置可不同，也可相同
- * 3）对 DiskFileItemFactory 对象设置一些 属性
- * 4）高水平的API文件上传处理  ServletFileUpload upload = new ServletFileUpload(factory);
- * 目的是调用 parseRequest（request）方法  获得 FileItem 集合list ，
- *     
- * 5）在 FileItem 对象中 获取信息，   遍历， 判断 表单提交过来的信息 是否是 普通文本信息  另做处理
- * 6）
- *    第一种. 用第三方 提供的  item.write( new File(path,filename) );  直接写到磁盘上
- *    第二种. 手动处理  
- *
+* 文件名称：TestUpload.java<br>
+* 摘要： 文件上传<br>
+* 具体步骤：<br>
+* 1） 获得磁盘文件条目工厂 DiskFileItemFactory 要导包<br>
+* 2） 利用 request 获取 真实路径 ，供临时文件存储，和 最终文件存储 ，这两个存储位置可不同，也可相同<br>
+* 3）对 DiskFileItemFactory 对象设置一些 属性<br>
+* 4）高水平的API文件上传处理  ServletFileUpload upload = new ServletFileUpload(factory);<br>
+* 	目的是调用 parseRequest（request）方法  获得 FileItem 集合list ，<br>
+*     
+* 5）在 FileItem 对象中 获取信息，   遍历， 判断 表单提交过来的信息 是否是 普通文本信息  另做处理<br>
+* 6）第一种. 用第三方 提供的  item.write( new File(path,filename) );  直接写到磁盘上<br>
+*   第二种. 手动处理<br>
+* -------------------------------------------------------<br>
+* 作者：胡毅<br>
+* 完成日期：2016年4月13日<br>
  */
 @WebServlet("/testUpload")
 public class TestUpload extends HttpServlet {
